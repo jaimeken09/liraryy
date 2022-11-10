d067598e5bc9e547f110da69a32ef161d9d5d837
jaime <jaimeken@yahoo.es>
2022-11-10 18:18:14 +0100

    rehaciendo.coger procesadore ir rehaciendobien.

---
+++ Usuario.java
@@ -0,0 +1,14 @@
+public class Usuario {
+    private Integer id;
+    public numeric numerousuario;
+    public String nombre;
+    pulic String apellidos;
+    public String poblacion;
+    private String direccion;
+
+    public Integer getId() {
+        return id;
+    }
+    public Integer setId() {
+        return id;
+    }
