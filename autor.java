41604668ede65635ce9b2f37682c51582f54ad4c
jaime <jaimeken@yahoo.es>
2022-11-11 10:17:45 +0100

    rehaciendo.coger procesadore ir rehaciendo bien. lalié y lo metí donde no era.

---
+++ autor.java
@@ -0,0 +1,25 @@
+public class autor {
+    public nombre;
+    public Integer getId() {
+        return id;
+        public apellidos;
+        public String get autor() {
+            return autor;
+
+
+        }
+
+    }
+}    public Integer setId() {
+    return id;
+    public void getId(Integer id) {
+        this.id = id;
+    };
+    public void getId(Integer id) {
+        this.id = id;
+    }
+
+
+
+}
+
